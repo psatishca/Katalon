5621be3c66dea835e9e8f1fb5b050cfd96fb6f86
preran.cadapa <preran.cadapa@macbookair.lan>
2025-01-12 10:10:02 +0530

    Project23 test suite added

---
+++ Test Suites/Test Suite 01.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite 01</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>d5c4cd70-c10e-4127-8087-804c9ba8e2e9</testSuiteGuid>
+</TestSuiteEntity>
